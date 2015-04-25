b43fdb4254eae36e6f8c1da92e41e2c4685d2084
rocantor <rjcantillo@misena.edu.co>
2015-04-25 17:28:22 +0000

    index listbox

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,6 @@
 class Member < ActiveRecord::Base
+    has_many :tasks
+    validates :name, presence: true
+    validates :lastname, presence: true
+    validates :email, presence: true
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ActiveRecord::Base
+    belongs_to :member
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :duration, presence: true
+    validates :duration, numericality: { only_integer: true }
+    validates :startdate, presence: true
+    validates :enddate, presence: true
+    
 end
