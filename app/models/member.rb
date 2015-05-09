af083293d387432252a135ba83f8ad9d235b1d94
rocantor <rjcantillo@misena.edu.co>
2015-05-09 22:45:20 +0000

    Internationalization_2

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,10 +4,12 @@ class Member < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
     has_many :tasks
+    validates :username, presence: true
     validates :name, presence: true
     validates :lastname, presence: true
     validates :email, presence: true
-    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }    
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create } 
+    validates :password, presence: true
 def fullname
   name + ' ' + lastname
 end
